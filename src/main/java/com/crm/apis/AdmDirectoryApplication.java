8c3281c746a284830fcbf4d6510bf35c1defeb28
Hicham Sassa <sass.hicham@gmail.com>
2020-05-09 23:14:41 +0200

    init

--- src/main/java/com/crm/apis/AdmDirectoryApplication.java
+++ src/main/java/com/crm/apis/AdmDirectoryApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.crm.apis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AdmDirectoryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(AdmDirectoryApplication.class, args);
     }
 
 }
